ccb8bf3b5d136222bfe2d0ea2a9e0809c9e1bb87
liuyang <me@liuyangjob.cn>
2019-08-27 18:02:05 +0800

    update

---
+++ myzkapi/src/main/java/cn/liuyangjob/zk/until/ZookeeperUtil.java
@@ -0,0 +1,15 @@
+package cn.liuyangjob.zk.until;
+
+/**
+ * Created by  liuyang
+ * 2019/8/27    17:44
+ * cn.liuyangjob.zk.util
+ * All Right Reserved by liuyang.
+ **/
+public class ZookeeperUtil {
+    /** zookeeper服务器地址 */
+    public static final String connectString = "113.142.70.112:2181";
+    /** 定义session失效时间 */
+    public static final int sessionTimeout = 5000;
+    public static final String path = "/root";
+}
