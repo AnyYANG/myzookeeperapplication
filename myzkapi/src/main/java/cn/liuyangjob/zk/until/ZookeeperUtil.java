e2456dce3bc518182bbbd77315db64246d4c5033
liuyang <me@liuyangjob.cn>
2019-08-28 10:04:35 +0800

    add zookeeperuntil

--- myzkapi/src/main/java/cn/liuyangjob/zk/until/ZookeeperUtil.java
+++ myzkapi/src/main/java/cn/liuyangjob/zk/until/ZookeeperUtil.java
@@ -1,5 +1,10 @@
 package cn.liuyangjob.zk.until;
 
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
 /**
  * Created by  liuyang
  * 2019/8/27    17:44
@@ -7,9 +12,32 @@ package cn.liuyangjob.zk.until;
  * All Right Reserved by liuyang.
  **/
 public class ZookeeperUtil {
-    /** zookeeper服务器地址 */
+    /**
+     * zookeeper服务器地址
+     */
     public static final String connectString = "113.142.70.112:2181";
-    /** 定义session失效时间 */
+    /**
+     * 定义session失效时间
+     */
     public static final int sessionTimeout = 5000;
+    /**
+     *  定义path
+     */
     public static final String path = "/root";
+
+
+    /**
+     * 返回一个zookeeper链接
+     * @param watcher
+     * @return
+     */
+    public static ZooKeeper getZookeeperConnection(Watcher watcher) {
+        ZooKeeper zooKeeper = null;
+        try {
+            zooKeeper = new ZooKeeper(connectString, sessionTimeout, watcher);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return zooKeeper;
+    }
 }
